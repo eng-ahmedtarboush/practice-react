735d9bc169bc4a1065e126b284cbcfa034138df4
Ahmed_Tarboush <20812021101005@eng.zu.edu.eg>
2023-01-17 17:45:43 +0200

    some edits

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('test',function () {
-//     return 'دلوفي اتعمل pull';
-// });
+Route::get('test',function () {
+    return 'دلوفي اتعمل pull';
+});

26657b12cf13d4953a1690f573338da16e81c70c
Ahmed_Tarboush <20812021101005@eng.zu.edu.eg>
2023-01-18 18:22:15 +0200

    some chances

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -15,67 +16,14 @@ class AuthController extends Controller
         $this->middleware('guest', ['only' => ['login', 'register']]);
     }
 
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-//        $validator = Validator::make($request->all(), [
-//            'name' => ['required', 'string', 'max:255'],
-//            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
-//            'password' => ['required', 'string', 'min:8', 'confirmed'],
-//            'phone_number' => ['required', 'string', 'unique:users,phone_number', 'size:11']
-//        ], [
-//            'name.required' => "الاسم مطلوب",
-//            'name.string' => "الاسم يجب ان يكون حروف",
-//            'name.max' => "أكبر عدد ممكن من الحروف هو 255 حرف",
-//
-//            'email.required' => "البريد الالكتروني مطلوب",
-//            'email.string' => "البريد الالكتروني يجب ان يكون حروف",
-//            'email.email' => "البرجاء ادخال البريد الالكتروني بشكل صحيح",
-//            'email.max' => "أكبر عدد ممكن من الحروف هو 255 حرف",
-//            'email.unique' => "البريد الالكتروني موجود مسبقا",
-//
-//            'password.required' => "كلمة المرور مطلوبة",
-//            'password.string' => "يجب أن تكون كلمة المرور عبارة عن نص",
-//            'password.min' => "أقل عدد ممكن من الحروف هو 8 حروف",
-//            'password.confirmed' => "كلمة السر غير متطابقة",
-//
-//            'phone_number.required' => "رقم الهاتف مطلوب",
-//            'phone_number.unique' => "رقم الهاتف موجود بالفعل",
-//            'phone_number.size' => "يجب أن يكون عدد الأرقام عن 11 رقم",
-//            'phone_number.min' => "يجب أن لا يقل عدد الأرقام عن 11 رقم",
-//            'phone_number.starts_with' => "يجب أن يبدأ رقم الهاتف بـ0",
-//        ]);
-//        if ($validator->fails()) {
-//            dd($validator->errors());
-//            return response()->json(['success'=>false, 'message'=>_('هناك خطأ في صحة البيانات'), 'data'=>$validator->errors()], 400);
-//        }
-//        $data = $validator->validated();
-
-        $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|email|unique:users',
-            'phone_number' => 'required|string|unique:users',
-            'password' => 'required|string|confirmed',
-        ],[
-            'name.required' => 'الاسم مطلوب',
-
-            'email.required' => 'البريد الالكتروني مطلوب',
-            'email.email' => 'البريد الالكتروني غير صحيح',
-            'email.unique' => 'البريد الالكتروني مستخدم من قبل',
-
-            'phone_number.required' => 'رقم الهاتف مطلوب',
-            'phone_number.unique' => 'رقم الهاتف مستخدم من قبل',
-
-            'password.required' => 'كلمة المرور مطلوبة',
-            'password.confirmed' => 'كلمة المرور غير متطابقة',
-        ]);
+        $data = $request->validated();
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone_number' => $request->phone_number,
-            'password' => bcrypt($request->password)
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'phone_number' => $data['phone_number'],
+            'password' => bcrypt($data['password'])
         ]);
         $token = $user->createToken('auth_token')->plainTextToken;
         return response()->json([
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'phone_number' => 'required|string|unique:users',
+            'password' => 'required|string|confirmed',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'الاسم مطلوب',
+
+            'email.required' => 'البريد الالكتروني مطلوب',
+            'email.email' => 'البريد الالكتروني غير صحيح',
+            'email.unique' => 'البريد الالكتروني مستخدم من قبل',
+
+            'phone_number.required' => 'رقم الهاتف مطلوب',
+            'phone_number.unique' => 'رقم الهاتف مستخدم من قبل',
+
+            'password.required' => 'كلمة المرور مطلوبة',
+            'password.confirmed' => 'كلمة المرور غير متطابقة',
+        ];
+    }
+}

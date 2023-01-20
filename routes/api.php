fadc53411df8ef612f56d2876c2dcd1b65b5bc46
Ahmed_Tarboush <20812021101005@eng.zu.edu.eg>
2023-01-20 15:13:36 +0200

    Solution Of Cors probles in last commit.. the solution is in config('cors.allowed_origins')

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::post('/user', function (Request $request) {
 Route::get('/', function () {
     return '';
 });
-Route::post('/auth/register', [AuthController::class , 'register'])->name('register');
+Route::post('/register', [AuthController::class , 'register'])->name('register');
 Route::post('/login', [AuthController::class , 'login'])->name('login');
 Route::post('/logout', [AuthController::class , 'logout'])->name('logout');

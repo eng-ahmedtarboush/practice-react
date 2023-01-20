35b778d9b27040d9b790173b68674dc259de3753
Ahmed_Tarboush <20812021101005@eng.zu.edu.eg>
2023-01-20 14:30:30 +0200

    some chances

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth.sanctum')->get('/user', function (Request $request) {
-    return $request->user()->only(['name', 'email', 'phone_number']);
+Route::get('/user', function (Request $request) {
+    return ['انا تماااام'];
+//    return $request->user()->only(['name', 'email', 'phone_number']);
 });
 Route::get('/', function () {
     return '';
